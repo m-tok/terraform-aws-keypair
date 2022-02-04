f5622d8db24c6e81c3bd96549b39db322e946911
m-tok <meneksetok1907@gmail.com>
2022-02-04 01:05:13 +0000

    publishing version1

--- example/module.tf
+++ example/module.tf
@@ -1,6 +1,3 @@
-### This module creates a key pair
-#### Please copy paste below code
-```
 module "dev" {
     source = "../"
     region = "us-east-1"
@@ -15,4 +12,3 @@ output region {
 output key_name {
     value = module.dev.key_name
 }
-```
